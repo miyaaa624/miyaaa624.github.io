14e1716e005156fa1a2ae54abdf55e48aeb07e7d
miyaaa624 <132032373+miyaaa624@users.noreply.github.com>
2023-06-01 18:07:50 +0800

    Delete 蔬果地網站 directory

--- 蔬果地網站/index2.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.btn-menu').on('click', function(){
-        $('.menu').addClass('active')
-    })
-    $('.btn-close').on('click', function(){
-        $('.menu').removeClass('active')
-    })
-})
